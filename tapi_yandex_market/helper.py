5c586954ec7c05481cefdfa4c45f4cddf92a4a72
pmaksimov <vur21@ya.ru>
2021-09-06 15:26:32 +0300

    fix iter_flatten_stats_row

--- tapi_yandex_market/helper.py
+++ tapi_yandex_market/helper.py
@@ -25,15 +25,16 @@ def iter_flatten_stats_row(
         'clicksMobile': 273,
         'showsMobile': '1225793',
         'spendingMobile': 31.19}]
-
-
-
     """
     for row in rows:
         for detailed_stats in row.pop("detailedStats", []):
             type_stats = detailed_stats.pop("type")
-            for metrika in list(detailed_stats.keys()):
-                metrika_name = metrika + type_stats.title()
-                metrika_name = new_field_names.get(metrika_name, metrika_name)
-                row[metrika_name] = detailed_stats.pop(metrika)
+            for field_name_detailed_stats in list(detailed_stats.keys()):
+                field_name = field_name_detailed_stats + type_stats.title()
+                row[field_name] = detailed_stats.pop(field_name_detailed_stats)
+
+        for field_name in list(row.keys()):
+            new_field_name = new_field_names.get(field_name, field_name)
+            row[new_field_name] = row.pop(field_name)
+
         yield row
